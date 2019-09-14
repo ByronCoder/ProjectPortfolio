ac137a8bd9ee8993ea153ac80c6dfac8488f354c
Byron Blank <byronbla@gmail.com>
2019-09-14 16:33:31 -0400

    Updates

---
+++ precache-manifest.478385666d749cf46b43e12d08d0f344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fac94ad51b2c5b1282d9b7c2855a49",
+    "url": "/ProjectPortfolio/index.html"
+  },
+  {
+    "revision": "07d0c18eefe81a2a645c",
+    "url": "/ProjectPortfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9cf602f72f155f2cee23",
+    "url": "/ProjectPortfolio/static/css/main.34dc2a07.chunk.css"
+  },
+  {
+    "revision": "07d0c18eefe81a2a645c",
+    "url": "/ProjectPortfolio/static/js/2.3be06df6.chunk.js"
+  },
+  {
+    "revision": "9cf602f72f155f2cee23",
+    "url": "/ProjectPortfolio/static/js/main.ce58f4ab.chunk.js"
+  },
+  {
+    "revision": "711606ff8d3090f0d420",
+    "url": "/ProjectPortfolio/static/js/runtime~main.9f8c75e8.js"
+  },
+  {
+    "revision": "9c1e251e6f93c2bb1ba3720dce027828",
+    "url": "/ProjectPortfolio/static/media/mypic.9c1e251e.jpeg"
+  }
+]);
